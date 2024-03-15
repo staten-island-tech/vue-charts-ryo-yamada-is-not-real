5e4f6b8145e9317a3e54ff9262a11c6c95ae253e
aalskdjfhg123 <jiangryan8@gmail.com>
2024-03-14 22:24:39 -0400

    im gonnaim gonnaim gonnaim gonnaim gonnaim gonnaim gonnaim gonnaim gonnaim gonna

---
+++ shesnotreal/src/components/squirrelData.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>{{ Squirrel.unique_squirrel_id }}</h2>
+        <h2>{{Squirrel.x}}, {{Squirrel.y}}</h2>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    Squirrel: Object,
+}) //if i change it from Squirrel to something else the code get very angry
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- shesnotreal/src/views/AboutView.vue
+++ shesnotreal/src/views/AboutView.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="map"></div>
+  <h1>
+    <div ref="map" class="map"></div>
+  </h1>
+  <squirrelData v-for="idiots in squirrels" :key="idiots.unique_squirrel_id" :-squirrel="idiots" />
 
 </template>
 
 <script setup>
-import { ref,onBeforeMount } from 'vue';
-import { Loader } from "@googlemaps/js-api-loader"
+import { ref, onBeforeMount, onMounted } from 'vue';
+import squirrelData from '@/components/squirrelData.vue';
 
 const squirrels = ref('')
-async function dox(){
+async function dox() {
   let slop = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=10")
   let data = await slop.json();
   squirrels.value = data;
   console.log(squirrels.value)
 }
-onBeforeMount(()=>
-dox())
 
-let map;
+const map = ref(null);
 
-async function initMap() {
+/* async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
@@ -27,28 +28,37 @@ async function initMap() {
     zoom: 8,
   });
 }
-
-initMap();
-const loader = new Loader({
-  apiKey: "YOUR_API_KEY",
-  version: "weekly",
-  ...additionalOptions,
-});
-
-loader.load().then(async () => {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+ */
+function makeMap() {
+  new window.google.maps.Map(map.value, {
+    center: { lat: 40.785091, lng: -73.968285 },
+    zoom: 15,
+    mapId: "4504f8b37365c3d0"
   });
-});
+}
+
+function makeMarker(){
+  new AdvancedMarkerElement({
+    map,
+    position: { lat: 40.785091, lng: -73.968285 },
+  });
+}
+
+onMounted(() =>
+  makeMap(),
+/*   makeMarker()
+ */)
+
+dox()
 
 
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"})
+
+
 </script>
 
 <style lang="scss" scoped>
-
+.map {
+  width: 500px;
+  height: 500px;
+}
 </style>
