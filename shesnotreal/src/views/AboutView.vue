5b01f1e88d828ecd132ff218041a59250bc5cc45
aalskdjfhg123 <jiangryan8@gmail.com>
2024-03-24 21:25:13 -0400

    pipebomb so cool

--- shesnotreal/src/views/AboutView.vue
+++ shesnotreal/src/views/AboutView.vue
@@ -6,67 +6,60 @@
     <div class="map">
   <GoogleMap v-if="loaded" api-key="AIzaSyAdB40-VhUUD6iG5RxtfbroGYMJk-Bn8m0" style="width: 500px; height: 500px" :center="center"
   :zoom="15">
-  <Marker :options="{ position: center }" />
     <Marker
         v-if="loaded"
         v-for="(idiots,i) in squ"
         :key="i"
-        :options="{ position: {lat: idiots.geocoded_column.coordinates[1], lng:idiots.geocoded_column.coordinates[0] }}"
-      />
+        :options="{ position: {lat: idiots.geocoded_column.coordinates[1], lng:idiots.geocoded_column.coordinates[0] }}">
+        <InfoWindow>
+        {{ idiots.unique_squirrel_id }}
+        <button class="pipebomb" @click="pipebomb(idiots)" >send pipebomb</button>
+      </InfoWindow>
+    </Marker>
 
   </GoogleMap> 
 </div>
   
-
-
-
 </template>
 
 <script>
-import { GoogleMap, Marker } from 'vue3-google-map'
+import { GoogleMap, Marker, InfoWindow } from 'vue3-google-map'
 import { ref, onBeforeMount, } from 'vue';
-import squirrelData from '@/components/squirrelData.vue';
-/* async function dox() {
-  let slop = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=10")
-  let data = await slop.json();
-  squirrels.value = data;
-  console.log(squirrels.value)
-}
- */
-
 
 export default {
   components:{
     GoogleMap,
-    Marker
+    Marker,
+    InfoWindow
   },
   data() {
     return {
       squ: [],
       loaded: false,
-      center: { lat: 40.78292600137983, lng: -73.9654160492904 }
-
+      center: { lat: 40.78292600137983, lng: -73.9654160492904 },
+      audio: new Audio("/explosionS.mp3"),
     }
   },
   onMounted: function () {
     this.dox()
   },
   methods: {
-
     dox: async function () {
-      try {
         let slop = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=10")
         let data = await slop.json();
         this.squ = data;
         console.log(this.squ)
         this.loaded = true
-      } catch (error) {
-
-      }
+      },
+      pipebomb: function(squirrel){
+        this.squ = this.squ.filter((t) => t !== squirrel)
+        this.audio.load()
+        this.audio.play()
+      } 
     }
 
   }
-}
+
 
 </script>
 
@@ -76,4 +69,5 @@ export default {
   top: 5vh;
   left: 5vw;
 }
+
 </style>
