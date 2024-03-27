c39a8550b542a190608c267b5336c79fac051f11
katherine <116115224+kaerulean@users.noreply.github.com>
2024-03-27 13:10:55 -0400

    peepeepoopoo

--- shesnotreal/src/views/AboutView.vue
+++ shesnotreal/src/views/AboutView.vue
@@ -1,15 +1,65 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <Bar v-if="loaded" :data="squirrels" />
+    <p v-else> p</p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data(){
+    return{
+      loaded:false,
+      squirrels: [],
+    }
+  },
+
+  mounted: function(){
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData: async function(){
+      try{
+        const result = await fetch(
+        'https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=10'
+        );
+        const data = await result.json();
+        this.squirrels = data;
+
+        this.loaded = true
+      }catch(error){
+      console.log(error)
+    }
+  }
+
+  // async mounted () {
+  //   this.loaded = false
+  //   try {
+  //     const userlist = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=10')
+  //     let squirrel = await userlist.json()
+  //     console.log(squirrel)
+  //     const hectare = []
+  //     squirrel.forEach((el) => hectare.push(el.hectare))//use hasownproperty for this instead
+  //     this.chartData.labels = hectare
+  //     // const sum = 0
+  //     // squirrel.forEach((el) => el.hectare+=sum)
+  //     // this.chartData.datasets = sum
+  //     const stuff = [0, 10, 20, 30, 40, 50, 60, 70, 80 , 90]
+  //     console.log(Object.getOwnPropertyNames(this.chartData.datasets)) //fix this, it's getting the first part of 0:1 aka 0 and not 1
+  //     this.chartData.datasets.data = hectare
+  //     this.loaded = true
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  // }
   }
 }
-</style>
+</script>
--- shesnotreal/src/views/HomeView.vue
+++ shesnotreal/src/views/HomeView.vue
@@ -1,9 +1,30 @@
+<template>
+  <div>
+    <test v-for="(poo) in squirrel" 
+    :key="poo.age"
+    :squirrel="poo"/>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import test from '@/components/barchart.vue'
+import {ref, onMounted} from 'vue'
+  const squirrel = ref(``)
+  async function getSquirrel(){
+    let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+    let data = await res.json()
+    squirrel.value = data
+    console.log(squirrel.value)
+    
+  }
+  
+  onMounted(()=>{
+    getSquirrel()
+  });
+  
+//yk how to loops thro the data maybe do that and put each into a data in the chart if that makes sense??
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
