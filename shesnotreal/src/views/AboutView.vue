26276545b798fbacbb4e203710f2796420a4c902
aalskdjfhg123 <jiangryan8@gmail.com>
2024-03-27 20:52:46 -0400

    nice argument but unfortunately -73.9565759733611 40.7992461674802

--- shesnotreal/src/views/AboutView.vue
+++ shesnotreal/src/views/AboutView.vue
@@ -1,19 +1,24 @@
 <template>
 
-<div class="doxButton">
-  <button @click="dox">dox </button>
-</div>
+
     <div class="map">
-  <GoogleMap v-if="loaded" api-key="AIzaSyAdB40-VhUUD6iG5RxtfbroGYMJk-Bn8m0" style="width: 500px; height: 500px" :center="center"
+  <GoogleMap api-key="AIzaSyAdB40-VhUUD6iG5RxtfbroGYMJk-Bn8m0" style="width: 35vw; height: 70vh; position: absolute; left: 32vw; top: 7vh;" :center="center"
   :zoom="15">
+  <CustomControl position="TOP_LEFT">
+      <button class="custom-btn" @click="dox">DOX</button>
+    </CustomControl>
     <Marker
         v-if="loaded"
         v-for="(idiots,i) in squ"
         :key="i"
         :options="{ position: {lat: idiots.geocoded_column.coordinates[1], lng:idiots.geocoded_column.coordinates[0] }}">
-        <InfoWindow class="info">
+        <InfoWindow class="info" style="font-family: Comic Sans MS, Comic Sans, cursive;">
         {{ idiots.unique_squirrel_id }}
-        <br><button class="pipebomb" @click="pipebomb(idiots)" >send pipebomb</button>
+        <br>
+        Age: {{ idiots.age }}
+        <br>
+        Color: {{ idiots.primary_fur_color }}
+        <br><button class="pipebomb" style="font-family: Comic Sans MS, Comic Sans, cursive;" @click="pipebomb(idiots)" >send pipebomb</button>
       </InfoWindow>
     </Marker>
 
@@ -23,12 +28,14 @@
 </template>
 
 <script>
-import { GoogleMap, Marker, InfoWindow } from 'vue3-google-map'
+import { GoogleMap, Marker, InfoWindow, CustomMarker, CustomControl  } from 'vue3-google-map'
 export default {
   components:{
     GoogleMap,
     Marker,
-    InfoWindow
+    InfoWindow,
+    CustomMarker,
+    CustomControl 
   },
   data() {
     return {
@@ -43,7 +50,7 @@ export default {
   },
   methods: {
     dox: async function () {
-        let slop = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=10")
+        let slop = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$limit=100")
         let data = await slop.json();
         this.squ = data;
         console.log(this.squ)
@@ -51,7 +58,6 @@ export default {
       },
       pipebomb: function(squirrel){
         this.squ = this.squ.filter((t) => t !== squirrel)
-        this.audio.volume = .1
         this.audio.load()
         this.audio.play()
       } 
@@ -63,10 +69,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.doxButton{
-  position: absolute;
-  top: 5vh;
-  left: 5vw;
+
+.custom-btn {
+  font-family: Comic Sans MS;
+  box-sizing: border-box;
+  background: white;
+  height: 40px;
+  width: 60px;
+  border-radius: 2px;
+  border: 0px;
+  margin: 10px;
+  padding: 0px;
+  font-size: 1.25rem;
+ 
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
+  overflow: hidden;
 }
 .pipebomb{
   margin-top: 1vh;
